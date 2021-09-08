0698803e869fca34642eb63e218b21d438947b80
gideonmandu <gideonmandu@gmail.com>
2021-09-08 19:42:44 +0300

    added set method for hashtable

--- src/hash_table.py
+++ src/hash_table.py
@@ -11,6 +11,18 @@ class HashTable:
             my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
         return my_hash
 
+    def set_item(self, key, value):
+        """Adds a key value pair to the hashtable
+
+        Args:
+            key ([type]): [description]
+            value ([type]): [description]
+        """
+        index = self.__hash(key)
+        if self.data_map[index] is None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
     def __str__(self):
         display = "{"
         for i, v in enumerate(self.data_map):
@@ -19,4 +31,8 @@ class HashTable:
 
 
 my_hash = HashTable()
+print(my_hash)
+my_hash.set_item("bolts", 12)
+my_hash.set_item("washers", 50)
+my_hash.set_item("screws", 10)
 print(my_hash)
