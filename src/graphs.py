89eb98dfc3d11ff763ecb1201f62ce8a1e573bde
gideonmandu <gideonmandu@gmail.com>
2021-09-09 20:17:30 +0300

    Added edge method

--- src/graphs.py
+++ src/graphs.py
@@ -11,13 +11,23 @@ class Graph:
             return True
         return False
 
+    def add_edge(self, v1, v2):
+        if (v1 and v2) in self.adj_list.keys(): 
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+
     def __str__(self):
-        graph = ""
+        graph = {}
         for vertex in self.adj_list:
-            graph += f"{vertex} : {self.adj_list[vertex]}, "
-        return graph
+            graph[vertex] = self.adj_list[vertex]
+        return str(graph)
 
 
 my_graph = Graph()
-my_graph.add_vertex("A")
+my_graph.add_vertex(1)
+my_graph.add_vertex(2)
+print(my_graph)
+my_graph.add_edge(1, 2)
 print(my_graph)
