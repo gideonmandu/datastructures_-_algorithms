6193d10de3895008dd687341cbe542882e8f2b90
gideonmandu <gideonmandu@gmail.com>
2021-09-09 20:33:50 +0300

    added method to remove edge

--- src/graphs.py
+++ src/graphs.py
@@ -12,22 +12,31 @@ class Graph:
         return False
 
     def add_edge(self, v1, v2):
-        if (v1 and v2) in self.adj_list.keys(): 
+        if (v1 and v2) in self.adj_list.keys():
             self.adj_list[v1].append(v2)
             self.adj_list[v2].append(v1)
             return True
         return False
 
+    def remove_edge(self, v1, v2):
+        if (v1 and v2) in self.adj_list.keys():
+            self.adj_list[v1].remove(v2)
+            self.adj_list[v2].remove(v1)
+            return True
+        return False
+
     def __str__(self):
-        graph = {}
-        for vertex in self.adj_list:
-            graph[vertex] = self.adj_list[vertex]
-        return str(graph)
+        return str({vertex: self.adj_list[vertex] for vertex in self.adj_list})
 
 
 my_graph = Graph()
-my_graph.add_vertex(1)
-my_graph.add_vertex(2)
+my_graph.add_vertex('A')
+my_graph.add_vertex('B')
+my_graph.add_vertex('C')
 print(my_graph)
-my_graph.add_edge(1, 2)
+my_graph.add_edge('A', 'B')
+my_graph.add_edge('B', 'C')
+my_graph.add_edge('C', 'A')
+print(my_graph)
+my_graph.remove_edge('A', 'B')
 print(my_graph)
