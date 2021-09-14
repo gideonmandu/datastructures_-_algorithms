fae467dcf2141b491aa29f746d0bbe9564f7b68e
gideonmandu <gideonmandu@gmail.com>
2021-09-14 19:08:48 +0300

    merging to pre sorted lists

---
+++ src/marge.py
@@ -0,0 +1,24 @@
+from rich import print
+
+
+def merge(sorted_list1: list, sorted_list2: list) -> list:
+    combined = []
+    i = 0
+    j = 0
+    while i < len(sorted_list1) and j < len(sorted_list2):
+        if sorted_list1[i] < sorted_list2[j]:
+            combined.append(sorted_list1[i])
+            i += 1
+        else:
+            combined.append(sorted_list2[j])
+            j += 1
+    while i < len(sorted_list1):
+        combined.append(sorted_list1[i])
+        i += 1
+    while j < len(sorted_list2):
+        combined.append(sorted_list2[j])
+        j += 1
+    return combined
+
+
+print(merge([1, 2, 3, 6], [9, 4, 7, 8]))
