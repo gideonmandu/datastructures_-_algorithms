94f0e49a7d3f7b6ab921fd0406a4929f90db922c
gideonmandu <gideonmandu@gmail.com>
2021-09-15 19:49:12 +0300

    added quicksort method

--- src/quicksort.py
+++ src/quicksort.py
@@ -27,3 +27,33 @@ def pivot(list_to_sort: list, pivot_index: int, end_index: int) -> int:
     swap(list_to_sort, pivot_index, swap_index)
     return swap_index
 
+
+def quick_sort_helper(list_to_sort: list, left: int, right: int):
+    """Sorts list from smallest to largest
+
+    Args:
+        list_to_sort (list): list to sort
+        left (int): left most index in list
+        right (int): right most index in list
+    """
+    if left < right:
+        pivot_index = pivot(
+            list_to_sort=list_to_sort, pivot_index=left, end_index=right
+        )
+        quick_sort_helper(
+            list_to_sort=list_to_sort, left=left, right=pivot_index-1
+        )
+        quick_sort_helper(
+            list_to_sort=list_to_sort, left=pivot_index+1, right=right
+        )
+    return list_to_sort
+
+
+def quick_sort(list_to_sort):
+    return quick_sort_helper(
+        list_to_sort=list_to_sort, left=0, right=len(list_to_sort)-1
+    )
+
+
+my_list = [4, 6, 1, 7, 3, 2, 5]
+print(quick_sort(my_list))
